888bde7bdb5dd1bd0f7d48117af1bfa1517259b2
Haifeng97 <zzzxxcvvbbb@163.com>
2024-12-24 23:58:02 +0800

    200

--- models/pidinet.py
+++ models/pidinet.py
@@ -331,12 +331,12 @@ class PiDiNet_6x4(nn.Module):
             block_class = PDCBlock
 
         # 通道设置
-        c1 = 100
-        c2 = 100
-        c3 = 100
-        c4 = 200
-        c5 = 400
-        c6 = 800
+        c1 = 200
+        c2 = 200
+        c3 = 200
+        c4 = 400
+        c5 = 800
+        c6 = 1600
 
         pdc_init = pdcs[0]
         self.init_block = Conv2d(pdc_init, 3, c1, kernel_size=3, padding=1)
